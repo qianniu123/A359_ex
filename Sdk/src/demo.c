64aa3b04b5bd49de05aaa1949215b7fd792e6cd8
qianniu123 <173518309@qq.com>
2020-12-08 23:04:06 +0800

    modify pb3 remap

--- Sdk/src/demo.c
+++ Sdk/src/demo.c
@@ -320,7 +320,9 @@ static void bsp_init(void)
 //----------------------------------------------------------------------
 //LED
 #if 1
-    //RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+	GPIO_PinRemapConfig(GPIO_Remap_SWJ_Disable, ENABLE);
     GPIO_InitStructure.GPIO_Pin = LED_PIN;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP; 
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
